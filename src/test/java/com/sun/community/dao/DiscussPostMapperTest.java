98967d8ee9d6e716a6b44d9a81f14595a72c2bf0
sun xiao <sunxiaoABC@outlook.com>
2021-12-06 16:04:41 +0800

    add logback-spring.xml

--- src/test/java/com/sun/community/dao/DiscussPostMapperTest.java
+++ src/test/java/com/sun/community/dao/DiscussPostMapperTest.java
@@ -1,35 +1,17 @@
 package com.sun.community.dao;
 
-import com.sun.community.CommunityApplication;
-import com.sun.community.entity.DiscussPost;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(classes = CommunityApplication.class)
+
 public class DiscussPostMapperTest {
-    @Autowired
-    private DiscussPostMapper mapper;
 
     @Test
     public void selectDiscussPosts() {
-        List<DiscussPost> list=mapper.selectDiscussPosts(0,0,10);
-        for (DiscussPost discussPost : list) {
-            System.out.println(discussPost);
-        }
     }
 
     @Test
     public void selectDiscussPostRows() {
-        System.out.println(mapper.selectDiscussPostRows(101));
     }
 }
