56acafffa2ef4e1925a10f1b5828449abb759c2b
SunXiao <sunxiaoABC@outlook.com>
2021-12-17 14:52:10 +0800

    add quartz configurations

---
+++ src/main/java/com/sun/community/config/QuartzConfig.java
@@ -0,0 +1,47 @@
+package com.sun.community.config;
+
+import com.sun.community.quartz.AlphaJob;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+//配置 -> 数据库 -> 调用
+@Configuration
+public class QuartzConfig {
+
+    // FactoryBean可简化Bean的实例化过程：
+    // 1.通过FactoryBean封装了Bean的实例化过程
+    // 2.将FactoryBean装配到Spring容器里
+    // 3.将FactoryBean注入给其他的Bean
+    // 4.该Bean得到的是FactoryBean所管理的对象实例
+
+    //通过返回JobDetailFactoryBean的方式实例化JobDetail
+   /* @Bean
+    public JobDetailFactoryBean alphaJobDetail() {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(AlphaJob.class);
+        factoryBean.setName("alphaJob");
+        factoryBean.setGroup("alphaJobGroup");
+        factoryBean.setDurability(true);
+        factoryBean.setRequestsRecovery(true);
+        return factoryBean;
+    }*/
+
+    //配置Trigger(SimpleTriggerFactoryBean, CronTriggerFactoryBean)
+    //将与参数同名的JobDetailFactoryBean实例化的Bean注入到JobDetail中
+   /* @Bean
+    public SimpleTriggerFactoryBean alphaTrigger(JobDetail alphaJobDetail) {
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(alphaJobDetail);
+        factoryBean.setName("alphaTrigger");
+        factoryBean.setGroup("alphaTriggerGroup");
+        factoryBean.setRepeatInterval(3000);
+        factoryBean.setJobDataMap(new JobDataMap());//存储Job状态
+
+        return factoryBean;
+    }*/
+
+}
